40da99e9f35b64747498ffa7c11ee111ef27270f
juan <dmsgo5866@gmail.com>
2025-09-05 15:13:03 +0900

    '업로드'

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const abilityBar = document.querySelectorAll('.ability-bar');
+const contacticon = document.querySelector('.contact__wrapper');
+console.log(abilityBar);
+console.log(contacticon);
+
+window.addEventListener('scroll', function(){
+  console.log(window.scrollY);
+  if (window.scrollY >= 700) {
+  abilityBar.forEach(function(abilityBarEl){
+    abilityBarEl.classList.add('show');
+  })
+  if(window.scrollY >= 2400){
+    contacticon.classList.add('show');
+  }
+  }
+})
+
+const swiper = new Swiper('.project .swiper', {
+  // 슬라이드 옵션 지정
+  direction: 'horizontal', // 수평 슬라이드
+  loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 다시 1
+  // autoplay: { // 자동 재생 여부
+  //   delay : 1000 // 1초마다 슬라이드 바뀜
+  // },
+
+  // 이전/다음 슬라이드 버튼 옵션
+  navigation: {
+    nextEl: '.project .swiper-button-next',
+    prevEl: '.project .swiper-button-prev',
+  },
+});
+
